fcd9af2c3c1669fd590b9c7ebddcb2aa4bbe9064
nadav41 <nadav4170@gmail.com>
2025-03-14 00:30:59 +0200

    Chatter!

--- app.py
+++ app.py
@@ -253,5 +253,5 @@ def process_dates():
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=5000, debug=False)
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+    # app.run(host="0.0.0.0", port=5001, debug=True)
